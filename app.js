80b8c5c40ddb45c8ecc419ce97175e0f2e4fb5e4
LongDao29 <Longsama2909@gmail.com>
2021-04-19 17:30:02 +0700

    make it better

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var multer = require('multer');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,7 +13,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
-
+app.use('/css', express.static(__dirname + '/css'));
+app.use('/assets', express.static(__dirname + '/assets'));
+app.use('/vendor', express.static(__dirname + '/vendor'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-// thay the duong dan mongo cua cac ban
 var urlDB = 'mongodb+srv://admin:long123@cluster0.1qo8p.mongodb.net/test';
 const mongoose = require('mongoose');
 mongoose.connect(urlDB, {useNewUrlParser: true, useUnifiedTopology: true});
@@ -9,128 +8,120 @@ mongoose.connect(urlDB, {useNewUrlParser: true, useUnifiedTopology: true});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
-  // we're connected!
-  console.log('connected!!!!')
+    // we're connected!
+    console.log('connected!!!!')
 });
 
-var multer = require('multer')
-var path = 'uploads/'
-var upload = multer({dest: path})
-// username
-// password
-// name
-// address
-// number_phone
-
-var user = new mongoose.Schema({
-  username: String,
-  password: String,
-  name: String,
-  address: String,
-  number_phone: String,
-  avatar: String
+const multer = require('multer');
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'assets/img/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
 })
+var upload = multer({storage:storage});
+
+
+
+const user = new mongoose.Schema({
+    username: String,
+    password: String,
+    name: String,
+    address: String,
+    number_phone: String,
+    avatar: String
+});
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  // ket noi toi collection ten la users
-  var connectUsers = db.model('users', user);
-  connectUsers.find({},
-      function (error, users) {
-        if (error) {
-          res.render('index', {title: 'Express : Loi@@@@'})
-        } else {
+    // ket noi toi collection ten la users
+    var connectUsers = db.model('users', user);
+    connectUsers.find({},
+        function (error, users) {
+            if (error) {
+                res.render('index', {title: 'Express : Loi@@@@'})
+            } else {
 
-          res.render('index', {title: 'Express', users: users})
-        }
-      })
+                res.render('index', {title: 'Express', users: users})
+            }
+        })
 });
 
-router.use('/css',express.static(__dirname+'/css'));
-router.use('/images',express.static(__dirname+'/images'));
-router.use('/vendor',express.static(__dirname+'/vendor'));
+
 router.post('/insertUser', upload.single('avatar'), function (req, res) {
-  console.log(req.body);
-  var connectUsers = db.model('users', user);
-  connectUsers({
-    username: req.body.username,
-    password: req.body.password,
-    name: req.body.name,
-    address: req.body.address,
-    number_phone: req.body.number_phone,
-    avatar: path + req.file.filename + '.jpg'
-  }).save(function (error) {
-    if (error) {
-      res.render('index', {title: 'Express Loi!!!!'});
-    } else {
-      res.redirect("/");
-    }
-  })
+    console.log(req.body);
+    var connectUsers = db.model('users', user);
+    connectUsers({
+        username: req.body.username,
+        password: req.body.password,
+        name: req.body.name,
+        address: req.body.address,
+        number_phone: req.body.number_phone,
+        avatar: req.file.filename
+    }).save(function (error) {
+        if (error) {
+            res.render('index', {title: 'Express Loi!!!!'});
+        } else {
+            res.redirect("/");
+        }
+    })
 });
-router.post('/updateUser', upload.single('avatar'), function (req, res) {
-  console.log(req.body);
-  var connectUsers = db.model('users', user);
-  connectUsers({
 
-    password: req.body.password,
-    name: req.body.name,
-    address: req.body.address,
-    number_phone: req.body.number_phone,
-    avatar: path + req.file.filename + '.jpg'
-  }).update({username: req.body.username},function (error) {
-    if (error) {
-      res.render('index', {title: 'Express Loi!!!!'});
-    } else {
-      res.redirect("/");
-    }
-  })
+router.post('/update', upload.single('avatar'), function (req, res) {
+    console.log(req.body);
+    var connectUsers = db.model('users', user);
+    connectUsers.update({username: req.body.username},{
+        password: req.body.password,
+        name: req.body.name,
+        address: req.body.address,
+        number_phone: req.body.number_phone,
+        avatar: req.file.filename
+    },function (error) {
+        if (error) {
+            res.render('index', {title: 'Express Loi!!!!'});
+        } else {
+            res.redirect("/");
+
+        }
+    })
 });
-// router.post('/:id', function (req, res) {
-//   console.log(req.body);
-//   var connectUsers = db.model('users', user);
-//   connectUsers.remove({_id: req.params.id}, function (error){
-//     if(error)
-//       throw error;
-//     else res.redirect("/")
-//     console.log("Xoa Thanh cong");
-//   })
-// });
+
 let baseJson = {
-  errorCode : undefined,
-  errorMessage: undefined,
-  data: undefined
+    errorCode: undefined,
+    errorMessage: undefined,
+    data: undefined
 }
-router.get('/getUsers', function (req, res){
-  var connectUsers = db.model('users', user);
-  connectUsers.find({},
-      function (error, users) {
-        if(error){
-          baseJson.errorCode = 400
-          baseJson.errorMessage = error
-          baseJson.data = []
-        }
-        else {
-          baseJson.errorCode = 200
-          baseJson.errorMessage = 'Thanh cong'
-          baseJson.data = users
-        }
-        res.send(baseJson)
-      })
+
+router.get('/getUsers', function (req, res) {
+    var connectUsers = db.model('users', user);
+    connectUsers.find({},
+        function (error, users) {
+            if (error) {
+                baseJson.errorCode = 400
+                baseJson.errorMessage = error
+                baseJson.data = []
+            } else {
+                baseJson.errorCode = 200
+                baseJson.errorMessage = 'Thanh cong'
+                baseJson.data = users
+            }
+            res.send(baseJson)
+        })
 });
 
 
 router.post('/:id', function (req, res) {
-  console.log(req.body);
-  var connectUsers = db.model('users', user);
-  connectUsers.remove({_id: req.params.id}, function (error){
-    if(error)
-      throw error;
-    else res.redirect("/")
-    console.log("Xoa Thanh cong");
-  })
+    console.log(req.body);
+    var connectUsers = db.model('users', user);
+    connectUsers.remove({_id: req.params.id}, function (error) {
+        if (error)
+            throw error;
+        else res.redirect("/")
+        console.log("Xoa Thanh cong");
+    })
 });
-
-
 
 
 module.exports = router;
